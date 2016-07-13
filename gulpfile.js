d1a381aab4376447066d84af2677219c4cc9a03b
Simon <simonxd@gmail.com>
2016-07-13 20:15:21 -0300

    initial

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+// third-party dependencies
+const gulp = require('gulp');
+const gulpSize = require('gulp-size');
+
+// browserify
+const browserify = require('browserify');
+const source     = require('vinyl-source-stream');
+const buffer     = require('vinyl-buffer');
+
+gulp.task('javascript', function () {
+  // set up the browserify instance on a task basis
+  var b = browserify({
+    entries: 'lib/index.js',
+    debug: true,
+
+    // standalone global object for main module
+    standalone: 'DOMHighlighter',
+  });
+
+  return b.bundle()
+    .pipe(source('dom-highlighter.js'))
+    .pipe(buffer())
+    .pipe(gulp.dest('dist'))
+    .pipe(gulpSize());
+});
+
+gulp.task('develop', ['javascript'], function () {
+
+  gulp.watch('lib/**/*.js', ['javascript']);
+
+});
